1bff00dc477618c83bf25310e76bd077866007f5
Milind Shah <milind.shah@thelangschool.net>
2024-09-24 15:02:42 -0400

    all methods

--- GroceryList/src/com/lang/grocerylist/GroceryList.java
+++ GroceryList/src/com/lang/grocerylist/GroceryList.java
@@ -1,6 +1,7 @@
 package com.lang.grocerylist;
 
 public class GroceryList {
+    ArrayList<String> list = new ArrayList<>();
     public void addItem(String item){}
     public void removeItem(String item){}
     public boolean findItem(String item){
