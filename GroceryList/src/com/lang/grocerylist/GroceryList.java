9af1ebe85044bfa21f197d844cec1b88e58d1e85
August Cornfeld <august.cornfeld@thelangschool.net>
2024-09-24 15:00:40 -0400

    Remove/add

--- GroceryList/src/com/lang/grocerylist/GroceryList.java
+++ GroceryList/src/com/lang/grocerylist/GroceryList.java
@@ -1,9 +1,17 @@
 package com.lang.grocerylist;
+import java.util.ArrayList;
 
 public class GroceryList {
-    public void addItem(String item){}
-    public void removeItem(String item){}
+    public void addItem(String item){
+        list.add(item);
+    }
+    public void removeItem(String item){
+        list.remove(item);
+    }
     public boolean findItem(String item){
+        if(list.indexOf(item)>0){
+            return true;
+        }
         return false;
     }
 }
